df47064f88a399ef2d7e582f6f3b603736f7e6da
nitanton6 <nitsoraanton@gmail.com>
2025-12-15 01:34:50 +0300

    LR2

---
+++ LR2/Task_2.py
@@ -0,0 +1,12 @@
+salary = 5000  # Ежемесячная зарплата
+spend = 6000  # Траты за первый месяц
+months = 10  # Количество месяцев, которое планируется протянуть без долгов
+increase = 0.03  # Ежемесячный рост цен
+start_month = 0
+money_capital = 0
+while start_month < months:
+    money_capital = money_capital + salary - spend
+    spend *= (1 + increase)
+    start_month += 1# TODO Рассчитайте подушку безопасности, чтобы протянуть 10 месяцев без долгов
+
+print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", -int(money_capital))
